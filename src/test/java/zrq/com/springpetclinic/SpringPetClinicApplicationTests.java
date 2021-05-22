51ef190464f04d994e247f364dd17cde8b85f769
Ricardo Quintas <rdquintas@yahoo.com>
2021-05-22 21:47:50 +0100

    initial build

---
+++ src/test/java/zrq/com/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package zrq.com.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
