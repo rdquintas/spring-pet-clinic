4104e76071e9da0179890946510df2586ecd0a75
Ricardo Quintas <rdquintas@yahoo.com>
2021-05-28 18:37:09 +0100

    Refactor Common Crud Interface

--- pet-clinic-data/src/main/java/zrq/com/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/zrq/com/springpetclinic/services/VetService.java
@@ -5,8 +5,6 @@ import zrq.com.springpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>  {
+
 }
