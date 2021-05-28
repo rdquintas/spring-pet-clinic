1f6b24ba59242f47e6258f7861ffc103977dcf78
Ricardo Quintas <rdquintas@yahoo.com>
2021-05-28 19:43:49 +0100

    Addub Map Service

---
+++ pet-clinic-data/src/main/java/zrq/com/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package zrq.com.springpetclinic.services.map;
+
+import zrq.com.springpetclinic.model.Pet;
+import zrq.com.springpetclinic.model.Vet;
+import zrq.com.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
